a4087ae01e7c100511a73db50b3efa78dec90e03
siruku@gmail.com <siruku@gmail.com>
2016-10-19 07:50:21 -0400

    Added ini logic for pulling credentials from file.6

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 from pytodoist import todoist
+import configparser, os
 
 class User():
-
-    projects = account.get_projects()
 
     def __init__(self, name):
         self.name = name
         self.email = ""
         self.password = ""
-
-    def set_email(self, email):
-        self.email = input("What is your email?")
-
-    def set_password(self, password):
-        self.password = input("What is your password?")
-
-    def login_user(self, email, password):
         self.account = todoist.login(self.email, self.password)
 
+    class Credentials():
 
-def loginUser(name):
-    
+        def __init__(self):
+            self.file = open('./todoist.ini', 'a+')
+            self.file.seek(0, 0)
+            print("size: " + str(os.stat('./todoist.ini').st_size))
+            self.Config = configparser.ConfigParser()
+            self.default = ['[Todoist Login Credentials]','\n','Username:','\n','Password:','\n']
 
+        def checkForINI(self):
+            if os.stat('./todoist.ini').st_size == 0:
+                print("Test1: File is empty.")
+                self.file.write("".join(self.default))
+                return self.file.close()
+            elif os.stat('./todoist.ini').st_size > len("".join(self.file)):
+                self.file.seek(0, 0)
+                print('Test3: File is larger than default.')
+                result = self.file.read().split()
+                print(len(result))
+                if len(result) < 5:
+                    print("Test4: File is missing info.")
+                    return None
+                for index, item in enumerate(result):
+                    print(str(index) + ": " + item)
+                User.email = result[2]
+                User.password = result[4]
+                return self.file.close()
+            return self.file.close()
 
-activeUser = createUser("Darius")
+activeUser = User('Darius')
 
-print(a.name)
+#User.Credentials.checkForINI(self)
+#print(User.email)
+#print(User.password)
+
+# print(activeUser.account.projects)
+# print(userCredentials.printConfig())
+# print(userCredentials.file)
+#print(len("".join(userCredentials.default)))
+
