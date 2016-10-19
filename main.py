8a72469f252116f9d403c4447f82021523768dea
Dstrasel <siruku@gmail.com>
2016-10-19 19:58:46 -0400

    Cleaning up config logic; will switch to ConfigParser

--- main.py
+++ main.py
@@ -9,38 +9,78 @@ class User():
         self.password = ""
         self.account = todoist.login(self.email, self.password)
 
-    class Credentials():
+class Config():
 
-        def __init__(self):
+    def __init__(self):
+        self.default = ['[Todoist Login Config]','\n','Username:','\n','Password:','\n']
+        self.file = self.processConfig() #open('./todoist.ini', 'a+')
+        self.isConfigLegal()
+        #self.file.seek(0, 0)
+        #print("size: " + str(os.stat('./todoist.ini').st_size))
+
+        #This will check if file is too big.
+        # elif os.stat('./todoist.ini').st_size > len("".join(self.file)):
+        #     self.file.seek(0, 0)
+        #     print('Test3: File is larger than default.')
+        #     result = self.file.read().split()
+        #     print(len(result))
+        #     if len(result) < 5:
+        #         print("Test4: File is missing info.")
+        #         return None
+        #     for index, item in enumerate(result):
+        #         print(str(index) + ": " + item)
+        #     User.email = result[2]
+        #     User.password = result[4]
+        #     return self.file.close()
+
+
+
+    def processConfig(self):
+        if self.configExist() == False:
+            self.createConfig()
+        elif self.isConfigLegal(createConfig()) == False:
+            self.createConfig(truncateFile=True)
+
+    def configExist(self):
+        return os.path.isfile('./todoist.ini')
+
+
+    def createConfig(self, truncateFile=False):
+        if truncateFile == False:
             self.file = open('./todoist.ini', 'a+')
-            self.file.seek(0, 0)
-            print("size: " + str(os.stat('./todoist.ini').st_size))
-            self.Config = configparser.ConfigParser()
-            self.default = ['[Todoist Login Credentials]','\n','Username:','\n','Password:','\n']
+            self.file.write("".join(self.default))
+            return self.file
+        self.file = open('./todoist.ini', 'w+')
+        self.file.write("".join(self.default))
+        return self.file
 
-        def checkForINI(self):
-            if os.stat('./todoist.ini').st_size == 0:
-                print("Test1: File is empty.")
-                self.file.write("".join(self.default))
-                return self.file.close()
-            elif os.stat('./todoist.ini').st_size > len("".join(self.file)):
-                self.file.seek(0, 0)
-                print('Test3: File is larger than default.')
-                result = self.file.read().split()
-                print(len(result))
-                if len(result) < 5:
-                    print("Test4: File is missing info.")
-                    return None
-                for index, item in enumerate(result):
-                    print(str(index) + ": " + item)
-                User.email = result[2]
-                User.password = result[4]
-                return self.file.close()
-            return self.file.close()
+    def isConfigLegal(self, file):
+        result = file.read().split()
+        if len(result) <= 3:
+            return False
+            print("Config has too many rows. Restoring to default.")
+            self.file = open('./todoist.ini', 'w+')
+            self.file.write("".join(self.default))
+        return True
 
-activeUser = User('Darius')
 
-#User.Credentials.checkForINI(self)
+
+
+    #def checkConfig(self):
+        #Check ini formatting(If length of ini is longer than Header, Usern
+
+
+            #configExist: See if a config file exists
+            #createConfig: Write a new config file
+            #checkConfig: Check config to see...? If empty, if default, or if username and password is present.
+            #getEmail: grab email address from config file
+            #getPassword: get password from config file
+
+
+#activeUser = User('Darius')
+newUser = Config()
+
+#User.Config.checkForINI(self)
 #print(User.email)
 #print(User.password)
 
